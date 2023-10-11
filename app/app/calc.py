dd25a54b045185d6f1a57e27081222cd8c26bc2d
rubenfh <ruben.falvert@edu.ece.fr>
2023-10-11 11:20:38 +0200

    Correcting calc and tests

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,7 @@
 """
-
 Calculator functions
 """
+
 
 def add(x, y):
     """ Add x and y and return result."""
@@ -11,4 +11,3 @@ def add(x, y):
 def subtract(x, y):
     """Subtract x from y and return result."""
     return y - x
-
